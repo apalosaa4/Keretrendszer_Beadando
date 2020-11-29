ede25ad4f5877e256118169477c720b512ccb0f9
Viktoria Bognar <apalosaa4@gmail.com>
2020-11-29 19:42:06 +0100

    Dao relational added

--- dao_relational/src/main/java/uni/eszterhazy/keretrendszer/dao/relational/TermekDAORelational.java
+++ dao_relational/src/main/java/uni/eszterhazy/keretrendszer/dao/relational/TermekDAORelational.java
@@ -1,4 +1,49 @@
 package uni.eszterhazy.keretrendszer.dao.relational;
 
-public class TermekDAORelational{
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import uni.eszterhazy.keretrendszer.dao.TermekDAO;
+import uni.eszterhazy.keretrendszer.model.Kategoria;
+import uni.eszterhazy.keretrendszer.model.Termek;
+
+import java.util.Collection;
+
+public class TermekDAORelational implements TermekDAO {
+
+    private static SessionFactory factory;
+
+    public TermekDAORelational() {
+        factory = new Configuration().configure().buildSessionFactory();
+    }
+
+    @Override
+    public void createTermek(Termek termek) {
+
+    }
+
+    @Override
+    public Collection<Termek> readAllTermek() {
+        return null;
+    }
+
+    @Override
+    public Termek readTermek() {
+        return null;
+    }
+
+    @Override
+    public void updateTermek(Termek termek) {
+
+    }
+
+    @Override
+    public void deleteTermek(Termek termek) {
+
+    }
+
+    @Override
+    public Collection<Termek> readAllTermekOfKategoria(Kategoria kategoria) {
+        return null;
+    }
 }
---
+++ dao_relational/src/test/java/uni/eszterhazy/keretrendszer/dao/relational/TermekDAORelationalTest.java
@@ -0,0 +1,15 @@
+package uni.eszterhazy.keretrendszer.dao.relational;
+
+import org.junit.Test;
+import uni.eszterhazy.keretrendszer.dao.TermekDAO;
+
+import static org.junit.Assert.*;
+
+public class TermekDAORelationalTest {
+
+    @Test
+    public void test(){
+        TermekDAO dao = new TermekDAORelational();
+    }
+
+}
