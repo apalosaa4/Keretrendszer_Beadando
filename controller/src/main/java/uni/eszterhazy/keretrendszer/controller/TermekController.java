e800da884205d9ccefa48bf0e144187dfc6ad686
Viktoria Bognar <apalosaa4@gmail.com>
2020-12-07 19:33:33 +0100

    DummyController added

--- controller/src/main/java/uni/eszterhazy/keretrendszer/controller/TermekController.java
+++ controller/src/main/java/uni/eszterhazy/keretrendszer/controller/TermekController.java
@@ -3,9 +3,10 @@ package uni.eszterhazy.keretrendszer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import uni.eszterhazy.keretrendszer.service.TermekService;
 
-@Component
+
 public class TermekController {
 
     @Autowired
